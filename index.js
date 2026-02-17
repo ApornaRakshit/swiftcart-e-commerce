c60da2ea9b1ede78d0c726b82f34d51db338e498
ApornaRakshit <rakshit.aporna@gmail.com>
2026-02-17 17:02:04 +0600

    Initialized the project

---
+++ index.js
@@ -0,0 +1,98 @@
+// Load All Categories
+const loadCategories = () => {
+  fetch("https://fakestoreapi.com/products/categories")
+    .then(res => res.json())
+    .then(json => displayCategories(json));
+};
+
+// Load All Products
+const loadAllProducts = () => {
+  fetch("https://fakestoreapi.com/products")
+    .then(res => res.json())
+    .then(data => displayProducts(data));
+};
+
+// Load Category Products
+const loadCategoryProduct = async (category) => {
+  const response = await fetch(
+    `https://fakestoreapi.com/products/category/${(category)}`
+  );
+
+  const data = await response.json();
+  displayProducts(data);
+};
+
+const displayCategories = (categories) => {
+  const levelContainer = document.getElementById("level-container");
+  levelContainer.innerHTML = "";
+
+  // All button
+  const allBtn = document.createElement("button");
+  allBtn.className = "btn btn-outline btn-primary";
+  allBtn.innerText = "All";
+  allBtn.addEventListener("click", loadAllProducts);
+  levelContainer.appendChild(allBtn);
+
+  for (let category of categories) {
+    let displayName =
+      category.charAt(0).toUpperCase() + category.slice(1);
+
+    const btn = document.createElement("button");
+    btn.className = "btn btn-outline btn-primary";
+    btn.innerText = displayName;
+
+    btn.addEventListener("click", () => {
+      loadCategoryProduct(category);
+    });
+
+    levelContainer.appendChild(btn);
+  }
+};
+
+// Display Products
+const displayProducts = (products) => {
+  const productContainer = document.getElementById("product-container");
+  productContainer.innerHTML = "";
+
+  for (let product of products) {
+    productContainer.innerHTML += `
+        <div class="card bg-base-100 shadow-lg rounded-xl overflow-hidden">
+          <figure class="bg-gray-100 p-6">
+            <img src="${product.image}" 
+                class="h-52 mx-auto object-contain"/>
+          </figure>
+
+          <div class="card-body">
+            <div class="flex justify-between items-center">
+              <span class="badge  text-blue-800 bg-blue-100">
+                ${product.category}
+              </span>
+              <span class="text-sm font-semibold">
+                <i class="fa-solid fa-star text-yellow-400"></i> 
+                ${product.rating.rate} (${product.rating.count})
+              </span>
+            </div>
+
+            <h2 class="card-title">
+              ${product.title.slice(0, 35)}...
+            </h2>
+
+            <p class="text-xl font-bold">$${product.price}</p>
+
+            <div class="card-actions justify-between mt-4">
+              <button class="btn btn-outline btn-sm">
+                <i class="fa-regular fa-eye"></i> Details
+              </button>
+              <button class="btn btn-primary btn-sm">
+                <i class="fa-solid fa-cart-shopping"></i> Add
+              </button>
+            </div>
+
+          </div>
+        </div>
+      `;
+  }
+};
+
+loadCategories();
+loadAllProducts();
